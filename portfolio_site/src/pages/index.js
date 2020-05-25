5ec2c5788838d0668dbc31ee944ab3a61792634d
Zaynaib Giwa <zaynaibg@gmail.com>
2020-05-24 22:22:02 -0500

    created skeleton gatsby site for portfolio

---
+++ portfolio_site/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Layout from "../components/layout"
+
+
+export default function Home() {
+  return (
+    <Layout>
+     
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </Layout>
+  )
+}

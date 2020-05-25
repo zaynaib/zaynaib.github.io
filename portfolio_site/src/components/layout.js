e52225372486ac9cefb916d2a9507069df208425
Zaynaib Giwa <zaynaibg@gmail.com>
2020-05-24 22:41:19 -0500

    created a talk component to portfolio site

--- portfolio_site/src/components/layout.js
+++ portfolio_site/src/components/layout.js
@@ -13,8 +13,9 @@ export default function Layout({ children }) {
   return (
     <div className={layoutStyles.layout}>
     <header style={{ marginBottom: `1.5rem` }}>
+    <h1>Hi I'm Ola</h1>
+
     <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-      <h3 style={{ display: `inline` }}>MySweetSite</h3>
     </Link>
     <ul style={{ listStyle: `none`, float: `right` }}>
       <ListLink to="/">Home</ListLink>
--- portfolio_site/src/pages/index.js
+++ portfolio_site/src/pages/index.js
@@ -2,11 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 import Layout from "../components/layout"
+import Talk from "../components/talks"
 
 
 export default function Home() {
   return (
     <Layout>
+
+      <Talk month="October" year= "2017" link="http://chicago.act-w.org/" name="ACT-W Chicktech Chicago" description= "Is FreeCodeCamp a Good Way to Learn How to Code? - Intro to Data Analysis with R"/>
      
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
